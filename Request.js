975908436481451b4f5c7f94448383383cf68216
Vitaliy <vitali_fc_arsenal@mail.ru>
2018-03-10 19:11:57 +0300

    add  destryctizatiy

--- Request.js
+++ Request.js
@@ -1,30 +1,30 @@
 "use strict"
 
-var methods = {
-    GET: "GET",
-    PUT: "PUT",
-    POST: "POST",
-    DELETE: "DELETE"
-};
+const [
+    GET = "GET",
+    PUT = "PUT",
+    POST = "POST",
+    DELETE = "DELETE"
+] = [];
 
 var url = "http://localhost:3000/posts/";
 function Get(id, func){
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = func;
-    xhr.open(methods.GET, url + id, true);
+    xhr.open(GET, url + id, true);
     xhr.send();
 }
 
 function GetAll(func){
     const xhr = new XMLHttpRequest();
     xhr.onreadystatechange = func;
-    xhr.open(methods.GET, url, true);
+    xhr.open(GET, url, true);
     xhr.send();
 }
 
 function Post(computer){
     const xhr = new XMLHttpRequest();
-    xhr.open(methods.POST, url, true);
+    xhr.open(POST, url, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(computer));
     alert("Saved");
@@ -32,7 +32,7 @@ function Post(computer){
 
 function Put(id, computer){
     const xhr = new XMLHttpRequest();
-    xhr.open(methods.PUT, url + id, true);
+    xhr.open(PUT, url + id, true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(JSON.stringify(computer));
     alert("Edit");
@@ -42,6 +42,6 @@ function Delete(id, func){
     if(confirm("Delete this computer?")){
         const xhr = new XMLHttpRequest();
         xhr.onreadystatechange = func;
-        xhr.open(methods.DELETE, url +"/"+ id, true);
+        xhr.open(DELETE, url +"/"+ id, true);
         xhr.send();
     }}
